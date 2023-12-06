b61ee59cac1640ca845ad32357eeeb76e1a7b963
Diego <diegopbetti@gmail.com>
2023-12-06 10:45:21 -0300

    adiciona as imagens dos dados

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,16 @@
 //Retorna um número aleatório entre 1 e 6
-function rolarDado(){
+function adicionaDado(){
     return Math.floor(Math.random() * 6) + 1;
+}
+
+// Retorna o caminho da imagem do dado
+function imagemDado(indice){
+    return spritesDados[indice];
+}
+
+// atualiza o valor do dado no HTML
+function atualizaDado(dadoTexto){
+    let dadoValor = adicionaDado();
+    dadoTexto.src = imagemDado(Valor - 1);
+    return Valor;
 }
