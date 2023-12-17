2f6895325e76e9777d04fa49d7c12226d3841acf
Diego <diegopbetti@gmail.com>
2023-12-17 16:17:02 -0300

    adiciona uma funcao que reinicia o jogo

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ import{
     terminaJogo,
     vencedor   
 } from './funcoes.js';
+
+let botColuna1 = 0, botColuna2 = 0, botColuna3 = 0;
+let playerColuna1 = 0, playerColuna2 = 0, playerColuna3 = 0;
+
+function reiniciarJogo() {
+    botColuna1 = botColuna2 = botColuna3 = 0;
+    playerColuna1 = playerColuna2 = playerColuna3 = 0;
+    jogadorAtual = 'Player';
+    atualizarInterface();
+  }
