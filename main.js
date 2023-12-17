eef59111f91c21a7df6fc8fd8fdb6a239ece27c7
Diego <diegopbetti@gmail.com>
2023-12-16 21:32:55 -0300

    importa as funcoes de funcoes.js para o main.js

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 //main.js
 
+import{
+    rolaDado,
+    imgDado,
+    novoDado,
+    adicionaDado,
+    numeroRepete,
+    soma,
+    imprimeSoma,
+    limpaNumeroDeColuna,
+    retornaTabuleiro,
+    terminaJogo,
+    vencedor   
+} from './funcoes.js';
