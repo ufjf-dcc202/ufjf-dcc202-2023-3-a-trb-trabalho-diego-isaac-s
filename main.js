446ead72d61e206369d8cb6ec95047f411dc3071
Diego <diegopbetti@gmail.com>
2023-12-17 21:35:23 -0300

    adiciona funcao que atualiza a interface

--- main.js
+++ main.js
@@ -17,9 +17,29 @@ import{
 let botColuna1 = 0, botColuna2 = 0, botColuna3 = 0;
 let playerColuna1 = 0, playerColuna2 = 0, playerColuna3 = 0;
 
+//reinicia o jogo
+
 function reiniciarJogo() {
     botColuna1 = botColuna2 = botColuna3 = 0;
     playerColuna1 = playerColuna2 = playerColuna3 = 0;
     jogadorAtual = 'Player';
     atualizarInterface();
-  }
+}
+
+//atualiza a interface
+
+function atualizarInterface() {
+    retornaTabuleiro(botColuna1, botColuna2, botColuna3);
+    retornaTabuleiro(playerColuna1, playerColuna2, playerColuna3);
+  
+    document.getElementById('valoresBotColuna1').textContent = soma([botColuna1, botColuna2, botColuna3]);
+    document.getElementById('valoresBotColuna2').textContent = soma([botColuna1, botColuna2, botColuna3]);
+    document.getElementById('valoresBotColuna3').textContent = soma([botColuna1, botColuna2, botColuna3]);
+  
+    document.getElementById('valoresPlayerColuna1').textContent = soma([playerColuna1, playerColuna2, playerColuna3]);
+    document.getElementById('valoresPlayerColuna2').textContent = soma([playerColuna1, playerColuna2, playerColuna3]);
+    document.getElementById('valoresPlayerColuna3').textContent = soma([playerColuna1, playerColuna2, playerColuna3]);
+  
+    proxDado.textContent = novoDado(document.querySelector('.dado'));
+    document.getElementById('nomePlayer').textContent = jogadorAtual;
+}  
